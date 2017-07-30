e85c48415dffe724a84629e4f2975dcda3d3b9b8
Conrad Stoll <conrad.stoll@mutualmobile.com>
2017-07-29 22:24:55 -0500

    Updating name

--- Snowman/Snowman/ViewController.swift
+++ Snowman/Snowman/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
