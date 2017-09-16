06b33cf5eacc3c087dd3c244e490695241499bee
Conrad Stoll <conrad.stoll@mutualmobile.com>
2017-09-15 19:26:36 -0500

    Adding About Page

---
+++ Snowman/Snowman/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  Snowman
+//
+//  Created by Conrad Stoll on 9/15/17.
+//  Copyright © 2017 Conrad Stoll. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutViewController : UIViewController {
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
